3551d3cb536e045f949c1b0a4258b3f3ca71dac5
Shamus Clifford <scliff108@gmail.com>
2020-04-29 19:54:58 -0400

    Updates

--- precache-manifest.4ae4490188f27aff92ecfc1552679634.js
+++ precache-manifest.4ae4490188f27aff92ecfc1552679634.js
@@ -1,390 +1,390 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16880e98a77a5c6d2a8f80d9840d034a",
-    "url": "/home-gym/index.html"
+    "revision": "35baa583e5ee8643f679a0a15cf5339e",
+    "url": "/index.html"
   },
   {
     "revision": "4e91954944c996c5e5ed",
-    "url": "/home-gym/static/css/2.7ad897a5.chunk.css"
+    "url": "/static/css/2.7ad897a5.chunk.css"
   },
   {
     "revision": "c48bfd2c77e8499605df",
-    "url": "/home-gym/static/css/main.d498bfb3.chunk.css"
+    "url": "/static/css/main.d498bfb3.chunk.css"
   },
   {
     "revision": "4e91954944c996c5e5ed",
-    "url": "/home-gym/static/js/2.c72b9b6e.chunk.js"
+    "url": "/static/js/2.c72b9b6e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/home-gym/static/js/2.c72b9b6e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c72b9b6e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "c48bfd2c77e8499605df",
-    "url": "/home-gym/static/js/main.50ffdb48.chunk.js"
+    "url": "/static/js/main.50ffdb48.chunk.js"
   },
   {
-    "revision": "1919770d47fec407e356",
-    "url": "/home-gym/static/js/runtime-main.31a45f65.js"
+    "revision": "9b2c46b77d2fbc0e275e",
+    "url": "/static/js/runtime-main.64057e84.js"
   },
   {
     "revision": "56e425593a50801a0a0a68adec21a257",
-    "url": "/home-gym/static/media/backFly1.56e42559.jpg"
+    "url": "/static/media/backFly1.56e42559.jpg"
   },
   {
     "revision": "2247b8af2bed8a0ce6590d2899259938",
-    "url": "/home-gym/static/media/backFly2.2247b8af.jpg"
+    "url": "/static/media/backFly2.2247b8af.jpg"
   },
   {
     "revision": "84d823a03a7ccda2ba75c07d2252a87c",
-    "url": "/home-gym/static/media/bentOverRow1.84d823a0.jpg"
+    "url": "/static/media/bentOverRow1.84d823a0.jpg"
   },
   {
     "revision": "6ddba1b94cdb30c0ba2ea96e4b85ed00",
-    "url": "/home-gym/static/media/bentOverRow2.6ddba1b9.jpg"
+    "url": "/static/media/bentOverRow2.6ddba1b9.jpg"
   },
   {
     "revision": "3cd074cff7723a63bd05601dc64f1ed9",
-    "url": "/home-gym/static/media/bicycle1.3cd074cf.jpg"
+    "url": "/static/media/bicycle1.3cd074cf.jpg"
   },
   {
     "revision": "802d49b230bcbbb8f9794310f74053d8",
-    "url": "/home-gym/static/media/bicycle2.802d49b2.jpg"
+    "url": "/static/media/bicycle2.802d49b2.jpg"
   },
   {
     "revision": "3cd074cff7723a63bd05601dc64f1ed9",
-    "url": "/home-gym/static/media/bicycle3.3cd074cf.jpg"
+    "url": "/static/media/bicycle3.3cd074cf.jpg"
   },
   {
     "revision": "7afc87826fb4b4fffa62501f3b96c62d",
-    "url": "/home-gym/static/media/bicycle4.7afc8782.jpg"
+    "url": "/static/media/bicycle4.7afc8782.jpg"
   },
   {
     "revision": "02baae383130bea30d3074c938976ce8",
-    "url": "/home-gym/static/media/bodyRow1.02baae38.jpg"
+    "url": "/static/media/bodyRow1.02baae38.jpg"
   },
   {
     "revision": "72d8fb20d3891e1fa0cfd83731838ccc",
-    "url": "/home-gym/static/media/bodyRow2.72d8fb20.jpg"
+    "url": "/static/media/bodyRow2.72d8fb20.jpg"
   },
   {
     "revision": "8a45325112484b8d25d4d32a41ef2a07",
-    "url": "/home-gym/static/media/broomTwist1.8a453251.jpg"
+    "url": "/static/media/broomTwist1.8a453251.jpg"
   },
   {
     "revision": "bd02b6f7d8c9da042dd6ee7505e91414",
-    "url": "/home-gym/static/media/broomTwist2.bd02b6f7.jpg"
+    "url": "/static/media/broomTwist2.bd02b6f7.jpg"
   },
   {
     "revision": "0f4967da8d944e028c3b5e6c903c5ac9",
-    "url": "/home-gym/static/media/broomTwist3.0f4967da.jpg"
+    "url": "/static/media/broomTwist3.0f4967da.jpg"
   },
   {
     "revision": "78adf8d2d38ce03628172dfe79504456",
-    "url": "/home-gym/static/media/burpee1.78adf8d2.jpg"
+    "url": "/static/media/burpee1.78adf8d2.jpg"
   },
   {
     "revision": "91b66b85dbcbd9084a284033b2bbb7a5",
-    "url": "/home-gym/static/media/burpee2.91b66b85.jpg"
+    "url": "/static/media/burpee2.91b66b85.jpg"
   },
   {
     "revision": "326c4eef36330cf574880fc48aacb560",
-    "url": "/home-gym/static/media/burpee3.326c4eef.jpg"
+    "url": "/static/media/burpee3.326c4eef.jpg"
   },
   {
     "revision": "f202a0731feff54f623d2d1453d09633",
-    "url": "/home-gym/static/media/burpee4.f202a073.jpg"
+    "url": "/static/media/burpee4.f202a073.jpg"
   },
   {
     "revision": "bdcf949af4ee12bea20e410b7f8b330f",
-    "url": "/home-gym/static/media/burpee5.bdcf949a.jpg"
+    "url": "/static/media/burpee5.bdcf949a.jpg"
   },
   {
     "revision": "322fb3bcb664d77e06e7aaa6b6910184",
-    "url": "/home-gym/static/media/burpee6.322fb3bc.jpg"
+    "url": "/static/media/burpee6.322fb3bc.jpg"
   },
   {
     "revision": "8a294588da1abec48d72d9816117d14f",
-    "url": "/home-gym/static/media/burpee7.8a294588.jpg"
+    "url": "/static/media/burpee7.8a294588.jpg"
   },
   {
     "revision": "e7e7b8d5d9ddd448f3c7e7153d708563",
-    "url": "/home-gym/static/media/calfRaise1.e7e7b8d5.jpg"
+    "url": "/static/media/calfRaise1.e7e7b8d5.jpg"
   },
   {
     "revision": "49e7999941381a914208b795bbe974f1",
-    "url": "/home-gym/static/media/calfRaise2.49e79999.jpg"
+    "url": "/static/media/calfRaise2.49e79999.jpg"
   },
   {
     "revision": "b0da4318b32d4dfa6fa9d4dc28f29452",
-    "url": "/home-gym/static/media/chestSqueeze1.b0da4318.jpg"
+    "url": "/static/media/chestSqueeze1.b0da4318.jpg"
   },
   {
     "revision": "b349bb19b144f97194673eb42f8aa2f0",
-    "url": "/home-gym/static/media/chestSqueeze2.b349bb19.jpg"
+    "url": "/static/media/chestSqueeze2.b349bb19.jpg"
   },
   {
     "revision": "cd290eb3237645e5f295b38d451d0dc2",
-    "url": "/home-gym/static/media/closeGrip1.cd290eb3.jpg"
+    "url": "/static/media/closeGrip1.cd290eb3.jpg"
   },
   {
     "revision": "c452b8da9e13e04598b47b64f92aa7e8",
-    "url": "/home-gym/static/media/closeGrip2.c452b8da.jpg"
+    "url": "/static/media/closeGrip2.c452b8da.jpg"
   },
   {
     "revision": "cf6d2db3fe6df49e8cfd640fa79c05ed",
-    "url": "/home-gym/static/media/deadLift1.cf6d2db3.jpg"
+    "url": "/static/media/deadLift1.cf6d2db3.jpg"
   },
   {
     "revision": "e8f5aec672d11adda0269ebfa1b0a0f2",
-    "url": "/home-gym/static/media/deadLift2.e8f5aec6.jpg"
+    "url": "/static/media/deadLift2.e8f5aec6.jpg"
   },
   {
     "revision": "6e63819b5d02f7b86c9527bdac1c860d",
-    "url": "/home-gym/static/media/decline1.6e63819b.jpg"
+    "url": "/static/media/decline1.6e63819b.jpg"
   },
   {
     "revision": "5447c8115d61062a1e72c7dd832e85fd",
-    "url": "/home-gym/static/media/decline2.5447c811.jpg"
+    "url": "/static/media/decline2.5447c811.jpg"
   },
   {
     "revision": "055481916a9691aa4ccdf255c420bec9",
-    "url": "/home-gym/static/media/dip1.05548191.jpg"
+    "url": "/static/media/dip1.05548191.jpg"
   },
   {
     "revision": "ec0fb8576eec63b18e0e4da9cafe33b2",
-    "url": "/home-gym/static/media/dip2.ec0fb857.jpg"
+    "url": "/static/media/dip2.ec0fb857.jpg"
   },
   {
     "revision": "f9847dc48110303b3b1da75ba1446e64",
-    "url": "/home-gym/static/media/doorSqueezeFly.f9847dc4.jpg"
+    "url": "/static/media/doorSqueezeFly.f9847dc4.jpg"
   },
   {
     "revision": "0d4733f210bf9bac6562fd749dd7c200",
-    "url": "/home-gym/static/media/dumbellCurl1.0d4733f2.jpg"
+    "url": "/static/media/dumbellCurl1.0d4733f2.jpg"
   },
   {
     "revision": "72fbe1cf54d5e0eac1d26da304fe91ac",
-    "url": "/home-gym/static/media/dumbellCurl2.72fbe1cf.jpg"
+    "url": "/static/media/dumbellCurl2.72fbe1cf.jpg"
   },
   {
     "revision": "716a390a1deeac8f04b810d07cc23a57",
-    "url": "/home-gym/static/media/easyDip1.716a390a.jpg"
+    "url": "/static/media/easyDip1.716a390a.jpg"
   },
   {
     "revision": "90082cc74f3e89b0f209e229afc7e159",
-    "url": "/home-gym/static/media/easyDip2.90082cc7.jpg"
+    "url": "/static/media/easyDip2.90082cc7.jpg"
   },
   {
     "revision": "f8e47a777df672e299720baa5611e539",
-    "url": "/home-gym/static/media/incline1.f8e47a77.jpg"
+    "url": "/static/media/incline1.f8e47a77.jpg"
   },
   {
     "revision": "f30ef4fecbc649b56373eaf5aecfc8ad",
-    "url": "/home-gym/static/media/incline2.f30ef4fe.jpg"
+    "url": "/static/media/incline2.f30ef4fe.jpg"
   },
   {
     "revision": "a0ee54f7bc4b87a4440691220de73570",
-    "url": "/home-gym/static/media/jeffersonCurl1.a0ee54f7.jpg"
+    "url": "/static/media/jeffersonCurl1.a0ee54f7.jpg"
   },
   {
     "revision": "8df94983cab9e4985007119619cd0519",
-    "url": "/home-gym/static/media/jeffersonCurl2.8df94983.jpg"
+    "url": "/static/media/jeffersonCurl2.8df94983.jpg"
   },
   {
     "revision": "cff7df4490e363ec930e50b2267107e5",
-    "url": "/home-gym/static/media/kneeBend1.cff7df44.jpg"
+    "url": "/static/media/kneeBend1.cff7df44.jpg"
   },
   {
     "revision": "4a7e85e0b3c783a209f83c0ed0ceebc6",
-    "url": "/home-gym/static/media/kneeBend2.4a7e85e0.jpg"
+    "url": "/static/media/kneeBend2.4a7e85e0.jpg"
   },
   {
     "revision": "e4bc94895e4ce3ab4f68a386b1812376",
-    "url": "/home-gym/static/media/latPulldown1.e4bc9489.jpg"
+    "url": "/static/media/latPulldown1.e4bc9489.jpg"
   },
   {
     "revision": "a1fc054f15b83de619be489582d4199e",
-    "url": "/home-gym/static/media/latPulldown2.a1fc054f.jpg"
+    "url": "/static/media/latPulldown2.a1fc054f.jpg"
   },
   {
     "revision": "689dc438bc78bcaaac17f85f122276c8",
-    "url": "/home-gym/static/media/lateralRaise1.689dc438.jpg"
+    "url": "/static/media/lateralRaise1.689dc438.jpg"
   },
   {
     "revision": "e69841003ececba3acafe2ed439f586c",
-    "url": "/home-gym/static/media/lateralRaise2.e6984100.jpg"
+    "url": "/static/media/lateralRaise2.e6984100.jpg"
   },
   {
     "revision": "5745a46bc3f89bf7c9a09f1eb5c83062",
-    "url": "/home-gym/static/media/legLift1.5745a46b.jpg"
+    "url": "/static/media/legLift1.5745a46b.jpg"
   },
   {
     "revision": "872a2d2a5f157ed1294800545fcfa303",
-    "url": "/home-gym/static/media/legLift2.872a2d2a.jpg"
+    "url": "/static/media/legLift2.872a2d2a.jpg"
   },
   {
     "revision": "0592d352162cc136f2f8099edc15cb2a",
-    "url": "/home-gym/static/media/lunge1.0592d352.jpg"
+    "url": "/static/media/lunge1.0592d352.jpg"
   },
   {
     "revision": "8e8391b969002f78accfff3cdaf5bf4d",
-    "url": "/home-gym/static/media/lunge2.8e8391b9.jpg"
+    "url": "/static/media/lunge2.8e8391b9.jpg"
   },
   {
     "revision": "0592d352162cc136f2f8099edc15cb2a",
-    "url": "/home-gym/static/media/lunge3.0592d352.jpg"
+    "url": "/static/media/lunge3.0592d352.jpg"
   },
   {
     "revision": "1be45592d8e097ba6dacc7d5f0b1f7d8",
-    "url": "/home-gym/static/media/lunge4.1be45592.jpg"
+    "url": "/static/media/lunge4.1be45592.jpg"
   },
   {
     "revision": "d4febe768dd437db1df99550db0e3119",
-    "url": "/home-gym/static/media/plank.d4febe76.jpg"
+    "url": "/static/media/plank.d4febe76.jpg"
   },
   {
     "revision": "b1c192227ddca57d409f831cc7e9ee4f",
-    "url": "/home-gym/static/media/pullUp1.b1c19222.jpg"
+    "url": "/static/media/pullUp1.b1c19222.jpg"
   },
   {
     "revision": "fae7a5ae612bb8244839c1a6ef95fa19",
-    "url": "/home-gym/static/media/pullUp2.fae7a5ae.jpg"
+    "url": "/static/media/pullUp2.fae7a5ae.jpg"
   },
   {
     "revision": "566fda7a485805859236fe8433f311b8",
-    "url": "/home-gym/static/media/pushUp1.566fda7a.jpg"
+    "url": "/static/media/pushUp1.566fda7a.jpg"
   },
   {
     "revision": "46942854e8e96a5e870c4cb864151bf9",
-    "url": "/home-gym/static/media/pushUp2.46942854.jpg"
+    "url": "/static/media/pushUp2.46942854.jpg"
   },
   {
     "revision": "f7efe9ead92942226dcbf5cc54d22d1b",
-    "url": "/home-gym/static/media/russianTwist1.f7efe9ea.jpg"
+    "url": "/static/media/russianTwist1.f7efe9ea.jpg"
   },
   {
     "revision": "0855d06a487aadfd38bb8764f77bbc6f",
-    "url": "/home-gym/static/media/russianTwist2.0855d06a.jpg"
+    "url": "/static/media/russianTwist2.0855d06a.jpg"
   },
   {
     "revision": "bcb389cdc4be29102520247dd7a32e36",
-    "url": "/home-gym/static/media/russianTwist3.bcb389cd.jpg"
+    "url": "/static/media/russianTwist3.bcb389cd.jpg"
   },
   {
     "revision": "416c71d8ae0e25fc7c16e841f32b9b69",
-    "url": "/home-gym/static/media/shoulderPress1.416c71d8.jpg"
+    "url": "/static/media/shoulderPress1.416c71d8.jpg"
   },
   {
     "revision": "9cc3029da04905674785316157471efd",
-    "url": "/home-gym/static/media/shoulderPress2.9cc3029d.jpg"
+    "url": "/static/media/shoulderPress2.9cc3029d.jpg"
   },
   {
     "revision": "81ec67135103bdd08bd3d3a85af1610b",
-    "url": "/home-gym/static/media/shoulderRaise1.81ec6713.jpg"
+    "url": "/static/media/shoulderRaise1.81ec6713.jpg"
   },
   {
     "revision": "82401bb914c752006b42c3f40b6b9207",
-    "url": "/home-gym/static/media/shoulderRaise2.82401bb9.jpg"
+    "url": "/static/media/shoulderRaise2.82401bb9.jpg"
   },
   {
     "revision": "3de959864d1bf81279254607c4073136",
-    "url": "/home-gym/static/media/shrug1.3de95986.jpg"
+    "url": "/static/media/shrug1.3de95986.jpg"
   },
   {
     "revision": "4d6a44fa056fcaeeec7fd0517305bcab",
-    "url": "/home-gym/static/media/shrug2.4d6a44fa.jpg"
+    "url": "/static/media/shrug2.4d6a44fa.jpg"
   },
   {
     "revision": "cfc1256a31804a57b0a91b0e247f56b1",
-    "url": "/home-gym/static/media/sidePlank.cfc1256a.jpg"
+    "url": "/static/media/sidePlank.cfc1256a.jpg"
   },
   {
     "revision": "28ef51df00825804cc916f8b03074e18",
-    "url": "/home-gym/static/media/sitUp1.28ef51df.jpg"
+    "url": "/static/media/sitUp1.28ef51df.jpg"
   },
   {
     "revision": "8191a42d9c224122e68ea068ea7bcb88",
-    "url": "/home-gym/static/media/sitUp2.8191a42d.jpg"
+    "url": "/static/media/sitUp2.8191a42d.jpg"
   },
   {
     "revision": "8bbade78cf5d183f06989644c05430d4",
-    "url": "/home-gym/static/media/skullCrusher1.8bbade78.jpg"
+    "url": "/static/media/skullCrusher1.8bbade78.jpg"
   },
   {
     "revision": "c7eeb683addca35961eaed702bc07186",
-    "url": "/home-gym/static/media/skullCrusher2.c7eeb683.jpg"
+    "url": "/static/media/skullCrusher2.c7eeb683.jpg"
   },
   {
     "revision": "473bbf4f2a38ff3fcdb2cf6697e60501",
-    "url": "/home-gym/static/media/squat1.473bbf4f.jpg"
+    "url": "/static/media/squat1.473bbf4f.jpg"
   },
   {
     "revision": "1f1cd11f7ac7c0f8bd542dab08d72d60",
-    "url": "/home-gym/static/media/squat2.1f1cd11f.jpg"
+    "url": "/static/media/squat2.1f1cd11f.jpg"
   },
   {
     "revision": "955a865916a988a5c775a78dcbb1e44a",
-    "url": "/home-gym/static/media/stepUp1.955a8659.jpg"
+    "url": "/static/media/stepUp1.955a8659.jpg"
   },
   {
     "revision": "227ec66c64650e39264168bcb42f15c9",
-    "url": "/home-gym/static/media/stepUp2.227ec66c.jpg"
+    "url": "/static/media/stepUp2.227ec66c.jpg"
   },
   {
     "revision": "552cad60c08103e313ce542eb138ac72",
-    "url": "/home-gym/static/media/stepUp3.552cad60.jpg"
+    "url": "/static/media/stepUp3.552cad60.jpg"
   },
   {
     "revision": "4e14a4732de9573ede3e025302e004f2",
-    "url": "/home-gym/static/media/superman1.4e14a473.jpg"
+    "url": "/static/media/superman1.4e14a473.jpg"
   },
   {
     "revision": "70b585de3f409ae5a6016154e7b8718b",
-    "url": "/home-gym/static/media/superman2.70b585de.jpg"
+    "url": "/static/media/superman2.70b585de.jpg"
   },
   {
     "revision": "d6bee3da9db10543cb768a0e9b883d8e",
-    "url": "/home-gym/static/media/tableDip1.d6bee3da.jpg"
+    "url": "/static/media/tableDip1.d6bee3da.jpg"
   },
   {
     "revision": "e0e2a95f142bae96a2477238566ff856",
-    "url": "/home-gym/static/media/tableDip2.e0e2a95f.jpg"
+    "url": "/static/media/tableDip2.e0e2a95f.jpg"
   },
   {
     "revision": "e9761b82b24a27de64b93eea2ad391f0",
-    "url": "/home-gym/static/media/toeTaps1.e9761b82.jpg"
+    "url": "/static/media/toeTaps1.e9761b82.jpg"
   },
   {
     "revision": "95ab931d796430ad192ffa274dc8dd8a",
-    "url": "/home-gym/static/media/toeTaps2.95ab931d.jpg"
+    "url": "/static/media/toeTaps2.95ab931d.jpg"
   },
   {
     "revision": "4d817b3796730bd7613392c3690210fb",
-    "url": "/home-gym/static/media/toeTaps3.4d817b37.jpg"
+    "url": "/static/media/toeTaps3.4d817b37.jpg"
   },
   {
     "revision": "541658ceeea22718d39b4a0cb2b31153",
-    "url": "/home-gym/static/media/tricepExtension1.541658ce.jpg"
+    "url": "/static/media/tricepExtension1.541658ce.jpg"
   },
   {
     "revision": "fa9bdf10fc4a5d95d752c078e5a80d35",
-    "url": "/home-gym/static/media/tricepExtension2.fa9bdf10.jpg"
+    "url": "/static/media/tricepExtension2.fa9bdf10.jpg"
   },
   {
     "revision": "ed420449e91eab1d3edc4881137a881c",
-    "url": "/home-gym/static/media/uprightRow1.ed420449.jpg"
+    "url": "/static/media/uprightRow1.ed420449.jpg"
   },
   {
     "revision": "526c317f7ef0e5fc84911368667ab92e",
-    "url": "/home-gym/static/media/uprightRow2.526c317f.jpg"
+    "url": "/static/media/uprightRow2.526c317f.jpg"
   },
   {
     "revision": "fd4b633843dff64b10277d2ba0f65cf6",
-    "url": "/home-gym/static/media/wideGrip1.fd4b6338.jpg"
+    "url": "/static/media/wideGrip1.fd4b6338.jpg"
   },
   {
     "revision": "715912f158d0118bf8ed11d8e06853a0",
-    "url": "/home-gym/static/media/wideGrip2.715912f1.jpg"
+    "url": "/static/media/wideGrip2.715912f1.jpg"
   }
 ]);
